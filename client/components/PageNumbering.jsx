d93b4326ee75c2be56d821cf8d3186b07209516a
Muneer <muneerhussain420420@gmail.com>
2025-02-19 15:51:48 +0500

    Paging integrated with backend

--- client/components/PageNumbering.jsx
+++ client/components/PageNumbering.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import { FaCaretRight } from "react-icons/fa";
 import { FaCaretLeft } from "react-icons/fa";
+import { useRouter } from "next/navigation";
 
 const PageNumbering = ({ currentPage, totalPages, setCurrentPage }) => {
+  const router= useRouter();
   return (
     <div className="flex justify-center gap-4 mt-5">
       <Button
         className="h-12 shadow-2xl bg-transparent border border-2 hover:bg-gradient-to-r from-[#00E8FC] via-[#D400A5] to-[#6A00F4]"
         disabled={currentPage == 1}
-        onClick={() => setCurrentPage(currentPage - 1)}
+        onClick={() => {setCurrentPage(currentPage - 1)
+          router.push(`/products?page=${currentPage-1}`)
+        }
+
+        }
       >
         <FaCaretLeft />
       </Button>
@@ -21,8 +27,13 @@ const PageNumbering = ({ currentPage, totalPages, setCurrentPage }) => {
       </Button>
       <Button
         className="h-12 shadow-2xl bg-transparent border border-2 hover:bg-gradient-to-r from-[#00E8FC] via-[#D400A5] to-[#6A00F4]"
-        disabled={currentPage + 1 > totalPages}
-        onClick={() => setCurrentPage(currentPage + 1)}
+        disabled={(currentPage + 1) > totalPages}
+        onClick={() => {
+          setCurrentPage(currentPage + 1)
+          router.push(`/products?page=${currentPage+1}`)
+        }
+
+        }
       >
         <FaCaretRight />
       </Button>
