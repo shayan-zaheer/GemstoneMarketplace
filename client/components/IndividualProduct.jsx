601d144a00b8589aa46292323f3bd61bc96e85f2
Muneer <muneerhussain420420@gmail.com>
2025-03-02 22:46:21 +0500

    Changes made 2

--- client/components/IndividualProduct.jsx
+++ client/components/IndividualProduct.jsx
@@ -2,6 +2,7 @@
 import axios from "axios";
 import { notFound } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 const IndividualProduct = ({ productID }) => {
   //   const [gem, setGem] = useState({})
@@ -29,18 +30,34 @@ const IndividualProduct = ({ productID }) => {
       "This breathtaking 1.5-carat diamond features a brilliant cut, exceptional clarity, and dazzling fire, making it the perfect centerpiece for an engagement ring or fine jewelry. Expertly crafted to maximize light reflection, this diamond offers unmatched sparkle and elegance. GIA certified for authenticity and quality, ensuring you receive a truly remarkable gem. Elevate your jewelry collection with this timeless beauty—secure it today!",
     createdAt: "2025-03-01T20:43:56.437Z",
     coverImage: "/diamondCover.jpg",
-    image: "/diamond.jpg"
+    image: "/diamond.jpg",
+    moreImages: [
+      "/diamond1.jpg",
+      "/diamond2.jpg",
+      "/diamond3.jpg",
+      "/diamond4.jpg",
+    ],
   };
   return (
     <div className="relative top-20 min-h-content mb-20 bg-[#1a1c1ff8] md:pl-12 md:flex md:flex-row-reverse">
-      <div className="md:w-[70%] h-[100vh] relative">
+      <motion.div
+        initial={{ x: 600 }}
+        animate={{ x: 0 }}
+        transition={{ type: "tween", duration: 0.3 }}
+        className="md:w-[68%] md:h-[800px]  relative"
+      >
         <img
           src={gem.coverImage}
           alt="Fading Image"
           className="w-full h-[300px] md:h-full  object-cover mask-gradient2 md:mask-gradient "
         />
-      </div>
-      <div className="md:w-[40%] md:pt-12 px-5 md:absolute md:z-10 md:left-10 md:top-12">
+      </motion.div>
+      <motion.div
+        initial={{ x: -600 }}
+        animate={{ x: 0 }}
+        transition={{ type: "tween", duration: 0.3 }}
+        className="md:w-[45%] md:pt-12 px-5 min-h-[350px] md:absolute md:z-10 md:left-10 md:top-12"
+      >
         <h1 className="bg-gradient-to-r from-[#00E8FC] via-[#D400A5] to-[#6A00F4] text-transparent bg-clip-text text-4xl md:text-6xl font-bold ">
           {gem.name}
         </h1>
@@ -52,10 +69,48 @@ const IndividualProduct = ({ productID }) => {
             {gem.description}
           </p>
         </div>
-        <div className="mt-8 text-white text-xl font-semibold">
-          {gem.createdAt}
+        <div className="mt-5 mb-12">
+          <span className="text-blue-400 font-semibold text-2xl">
+            More Images
+          </span>
+          <div className="flex flex-row w-full flex-wrap justify-start mt-3 gap-3">
+            {gem.moreImages.map((image, index) => {
+              if (index == 2 && gem.moreImages.length > 3) {
+                return (
+                  <div
+                    key={index}
+                    className="relative  md:w-32 md:h-32 w-24 h-24 border rounded-lg hover:cursor-pointer hover:brightness-[80%]"
+                  >
+                    <img
+                      src="/diamond3.jpg"
+                      alt="image3"
+                      className="w-full h-full rounded-lg"
+                    />
+                    <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center rounded-lg">
+                      <span className="text-white text-3xl font-normal">
+                        +{gem.moreImages.length - 3}
+                      </span>
+                    </div>
+                  </div>
+                );
+              } else if (index < 2) {
+                return (
+                  <img
+                    key={index}
+                    src={image}
+                    alt="image"
+                    className="w-24 h-24 md:w-32 md:h-32  rounded-lg border hover:cursor-pointer hover:brightness-[80%]"
+                  />
+                );
+              }
+            })}
+          </div>
         </div>
-      </div>
+
+        {/* <div className="mt-8 text-white text-xl font-semibold italic">
+          Uploaded on {new Date(gem.createdAt).toLocaleString()}
+        </div> */}
+      </motion.div>
     </div>
   );
 };
