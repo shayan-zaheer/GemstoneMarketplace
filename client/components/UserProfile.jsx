9697398bb0b06c4ce5132978312681ac81cebf88
Muneer <muneerhussain420420@gmail.com>
2025-03-10 16:16:10 +0500

    Badges(Active, Inactive) added

--- client/components/UserProfile.jsx
+++ client/components/UserProfile.jsx
@@ -13,62 +13,6 @@ const getUserById = async(id) => {
 const UserProfile = async({ userid }) => {
   const user = await getUserById(userid);
   console.log(user);
-  // const user = {
-  //   userid: 2,
-  //   userName: "Shayan Zaheer",
-  //   profileImage: "/user-profile.png",
-  //   userBio:
-  //     "100% Authentic Gemstone seller. Sold over 10+ certified gemstones",
-  //   ownedGemstones: [
-      
-  //   ],
-  //   soldGemstones: [{
-  //     id: 2,
-  //     name: "Diamond",
-  //     price: "200ETH",
-  //     owner: {
-  //       name: "Shayan",
-  //     },
-  //     image: "/diamond.jpg"
-  //   },
-  //   {
-  //     id: 3,
-  //     name: "Diamond",
-  //     price: "200ETH",
-  //     owner: {
-  //       name: "Shayan",
-  //     },
-  //     image: "/diamond.jpg"
-  //   },
-  //   {
-  //     id: 4,
-  //     name: "Diamond",
-  //     price: "200ETH",
-  //     owner: {
-  //       name: "Shayan",
-  //     },
-  //     image: "/diamond.jpg"
-  //   },
-  //   {
-  //     id: 5,
-  //     name: "Diamond",
-  //     price: "200ETH",
-  //     owner: {
-  //       name: "Shayan",
-  //     },
-  //     image: "/diamond.jpg"
-  //   },
-  //   {
-  //     id: 6,
-  //     name: "Diamond",
-  //     price: "200ETH",
-  //     owner: {
-  //       name: "Shayan",
-  //     },
-  //     image: "/diamond.jpg"
-  //   },
-  //   ],
-  // };
   return (
     <div className="min-h-content relative top-20 mb-20 bg-[#1a1c1ff8]">
       <div className="flex flex-col md:flex-row gap-[0.06rem]  bg-gradient-to-b from-[#00E8FC] via-[#D400A5] to-[#6A00F4]">
@@ -87,10 +31,24 @@ const UserProfile = async({ userid }) => {
               <span className="flex items-center justify-center text-gray-300 italic text-md leading-tight mt-2 font-medium">
                 {user.residenceAddress}
               </span>
-              <span className="flex items-center justify-center text-gray-300 italic text-md leading-tight mt-2 font-medium">
+              <span className="flex items-center justify-center text-gray-300 italic text-sm leading-tight mt-2 font-medium">
                 {user.walletAddress}
               </span>
             </div>
+              <div className="flex gap-6 mt-5 ">
+                  <div className="flex border gap-1 px-2 items-center border-gray-700 rounded-lg">
+                    <span className="w-1 h-1  rounded-full border border-4 border-green-500"></span>
+                    <span className="text-gray-300">Active: 5</span>
+                  </div>
+                  <div className="flex border gap-1 px-2 items-center border-gray-700 rounded-lg">
+                    <span className="w-1 h-1  rounded-full border border-4 border-red-500"></span>
+                    <span className="text-gray-300">Inactive: 2</span>
+                  </div>
+                  <div className="flex border gap-1 px-2 items-center border-gray-700 rounded-lg">
+                    <span className="w-1 h-1  rounded-full border border-4 border-blue-500"></span>
+                    <span className="text-gray-300">Sold: 5</span>
+                  </div>
+              </div>
           </div>
         </div>
         <div className="md:w-[60%] bg-[#1a1c1ff8] px-5 min-h-content">
